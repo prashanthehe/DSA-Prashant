b6a5f08db97a7d4f7a787e12d9bc98ca9a4540e3
prashanthehe <memotoprashant@gmail.com>
2026-02-18 11:43:59 +0530

    add initial string manipulation functions and examples

---
+++ string.py
@@ -0,0 +1,19 @@
+name = "PrashantStar"
+print(name[6])
+print(name[::-1])
+print(name[5::])
+for _ in name:
+    print(_,end="")
+print()
+
+s = "12345"
+
+l1 = ["A", "B", "C"]
+print(" ".join(l1))
+s = "Prashant Star"
+# s.split(" ")
+
+
+
+# s = sum([int(v) for v in s])
+# print(sum(s))
