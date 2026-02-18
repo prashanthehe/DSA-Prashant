ec51422653c5b9e7b2db1f66623510fcf71522cc
prashanthehe <memotoprashant@gmail.com>
2026-02-18 12:07:32 +0530

    add function definitions for basic arithmetic and printing operations

---
+++ function.py
@@ -0,0 +1,26 @@
+def add(a,b,c = 0):
+    return a+b+c
+
+# result = add(10,20)
+# print(result)
+
+# result = add(10,20,30)
+# print(result)
+
+def Name():
+    print("HEHEHEHEHE")
+
+# print(Name())
+
+def funPrint(a,b = None,c = None):
+    print("a :- ",a)
+    print("b :- ",b)
+    print("c :- ",c)
+
+
+t1 = 10,
+
+def sumOfNNumber(t):
+    print(sum(t))
+
+sumOfNNumber([10,20,30,40,50,60])
